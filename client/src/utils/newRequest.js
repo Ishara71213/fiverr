f02383cc8d7639d9fe1cff45b6502611c44837b2
Madushanka Ishara <madushankaishara48@gmail.com>
2023-04-03 16:30:54 +0530

    new req change

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -4,8 +4,8 @@ const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 const token = currentUser?.tokenAuth;
 
 const newRequest = axios.create({
-  // baseURL: "https://fiverr-testapi.onrender.com/api/",
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://fiverr-testapi.onrender.com/api/",
+  // baseURL: "http://localhost:8800/api/",
   headers: { Authorization: `${token}` },
   withCredentials: true,
 });
